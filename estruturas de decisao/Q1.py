7fbca914b8f71ba6d275c897033e6c7ae4943a6c
divinojun <112199216+divinojun@users.noreply.github.com>
2022-09-02 09:27:04 -0300

    Novos exercicios

---
+++ estruturas de decisao/Q1.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+n1 = int(input("1º número: "))
+n2 = int(input("2º número: "))
+if n1 > n2:
+    print('O 1º é maior')
+else:
+    print('O 2º é maior')
